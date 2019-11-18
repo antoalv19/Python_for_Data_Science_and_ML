5bbad19dee08d673a5b2bf9b3d59ec8374c202e3
Antonio <antonioalvino19@gmail.com>
2019-11-18 17:22:13 +0100

    Fine esercizi Crash Course - Review Effettuata

--- Py_Crash_Course.py
+++ Py_Crash_Course.py
@@ -46,6 +46,8 @@ domainGet("user@domain.com")
 
 # Create a basic function that returns True if the word 'dog' is contained in the input string.
 # Don't worry about edge cases like a punctuation being attached to the word dog, but do account for capitalization.
+# def findDog(st):
+#    return 'dog' in st.lower().split()
 
 
 def is_there_dog(dog):
@@ -56,6 +58,14 @@ def is_there_dog(dog):
 is_there_dog("Dog")
 
 # Create a function that counts the number of times the word "dog" occurs in a string. Again ignore edge cases.
+"""
+def countDog(st):
+    count = 0
+    for word in st.lower().split():
+        if word == 'dog':
+            count += 1
+    return count
+"""
 
 
 def how_many_dogs(dog):
@@ -87,6 +97,22 @@ results: "No ticket", "Small ticket", or "Big Ticket". If your speed is 60 or le
 speed is between 61 and 80 inclusive, the result is "Small Ticket". If speed is 81 or more, the result is "Big 
 Ticket". Unless it is your birthday (encoded as a boolean value in the parameters of the function) -- on your 
 birthday, your speed can be 5 higher in all cases.
+"""
+
+"""
+def caught_speeding(speed, is_birthday):
+    
+    if is_birthday:
+        speeding = speed - 5
+    else:
+        speeding = speed
+    
+    if speeding > 80:
+        return 'Big Ticket'
+    elif speeding > 60:
+        return 'Small Ticket'
+    else:
+        return 'No Ticket'
 """
 
 
