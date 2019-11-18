0cae4972679d064b0e1c17fa0fda9309813f674a
Antonio <antonioalvino19@gmail.com>
2019-11-18 17:13:22 +0100

    Fine esercizi Crash Course

--- Py_Crash_Course.py
+++ Py_Crash_Course.py
@@ -77,3 +77,35 @@ how_many_dogs("dog dog dog ,cane, dog, oooo")
 
 seq = ['soup', 'dog', 'salad', 'cat', 'great']
 
+filtered_seq = list(filter(lambda x: x[0] == "s", seq))
+
+print(filtered_seq)
+
+"""
+You are driving a little too fast, and a police officer stops you. Write a function to return one of 3 possible 
+results: "No ticket", "Small ticket", or "Big Ticket". If your speed is 60 or less, the result is "No Ticket". If 
+speed is between 61 and 80 inclusive, the result is "Small Ticket". If speed is 81 or more, the result is "Big 
+Ticket". Unless it is your birthday (encoded as a boolean value in the parameters of the function) -- on your 
+birthday, your speed can be 5 higher in all cases.
+"""
+
+
+def caught_speeding(speed, is_birthday):
+    if is_birthday:
+        if speed <= 65:
+            print("No Ticket")
+        elif 66 <= speed <= 86:
+            print("Small Ticket")
+        else:
+            print("Big Ticket")
+    else:
+        if speed < 60:
+            print("No Ticket")
+        elif 61 <= speed <= 81:
+            print("Small Ticket")
+        else:
+            print("Big Ticket")
+
+
+caught_speeding(61, False)
+
