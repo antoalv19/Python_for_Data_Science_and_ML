f6b6438e770c8326ae275c946ff7cd5e22ca7ced
Antonio <antonioalvino19@gmail.com>
2019-11-22 16:02:24 +0100

    Practice section Pandas

--- Pandas_Practice.py
+++ Pandas_Practice.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from numpy.random import randn
 
 # Creating a pandas series - You can convert a list,numpy array, or dictionary to a Series:
 labels = ["a", "b", "c"]
@@ -27,4 +28,30 @@ ser2 = pd.Series([1, 2, 5, 4], index=["USA", "Germany", "Italy", "Japan"])
 
 # Operations are then also done based off of index:
 operation = ser1 + ser2
-print(operation)
+# print(operation)
+
+# Set pandas dataframe environment
+np.random.seed(101)
+df = pd.DataFrame(np.random.rand(5, 4), index=["A", "B", "C", "D", "E"], columns=["W", "X", "Y", "Z"])
+df
+
+# Selection and Indexing
+df["W"]
+
+# Pass a list of column names
+df[["W", "Y"]]
+
+# SQL syntax
+df.W
+
+# DF columns are series
+type(df["W"])
+
+# Creating a new column
+df["New"] = df["W"] + df["Y"]
+
+# Removing columns - Specify Inplace = True
+df.drop("New", axis=1, inplace=True)
+
+
+
