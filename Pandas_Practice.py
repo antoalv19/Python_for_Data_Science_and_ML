f7664529446c595ddb199633bef83a9916dcba21
Antonio <antonioalvino19@gmail.com>
2019-11-25 13:38:48 +0100

    Practice section Pandas - fine

--- Pandas_Practice.py
+++ Pandas_Practice.py
@@ -33,7 +33,6 @@ operation = ser1 + ser2
 # Set pandas dataframe environment
 np.random.seed(101)
 df = pd.DataFrame(np.random.rand(5, 4), index=["A", "B", "C", "D", "E"], columns=["W", "X", "Y", "Z"])
-df
 
 # Selection and Indexing
 df["W"]
@@ -54,8 +53,62 @@ df["New"] = df["W"] + df["Y"]
 df.drop("New", axis=1, inplace=True)
 
 # Drop rows specifying index = 0
-df.drop("E", axis=0, inplace=True)
-print(df)
+# df.drop("E", axis=0, inplace=True)
 
+# Selecting Rows
+df.loc["A"]
 
+# Or select based off of position instead of label
+df.iloc[2]
 
+# Selecting subset of rows and columns
+df.loc["B", "Y"]
+
+df.loc[["A", "B"], ["W", "Y"]]
+
+# Conditional Selection
+df > 0
+
+df[df > 0]
+
+df[df["W"] > 0]
+
+df[df["W"] > 0]["Y"]
+
+df[df["W"] > 0][["Y", "X"]]
+
+# For two conditions you can use | - OR and & - AND with parenthesis
+# df[(df["W"] > 0) & (df[df["Y"] > 0])]
+
+# More Index Details
+# Let's discuss some more features of indexing, including resetting the index or setting it something else.
+# Reset index to default
+df.reset_index()
+
+# New Index
+newind = "CA NY WY OR CO".split()
+
+# New column
+df["States"] = newind
+
+# Set new  index
+df.set_index("States", inplace=True)
+
+# Multi-Index and Index Hierarchy
+outside = ['G1', 'G1', 'G1', 'G2', 'G2', 'G2']
+inside = [1, 2, 3, 1, 2, 3]
+hier_index = list(zip(outside, inside))
+hier_index = pd.MultiIndex.from_tuples(hier_index)
+
+# New dataframe
+
+df = pd.DataFrame(np.random.randn(6, 2), index=hier_index, columns=['A', 'B'])
+
+# Give name to indexes
+df.index.names = ["Group", "Num"]
+
+# Select using XS - da indice esterno a indice interno
+df.xs(["G1", 1])
+
+# Select using XS - da indice interno a esterno
+df.xs(1, level="Num")
